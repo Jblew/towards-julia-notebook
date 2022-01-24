61a8ad313ada4e713bd43a33cd2b6a027f11a5b8
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-01-24 14:11:38 +0100

    fix: gcp unable to build

--- gcp.config.sh
+++ gcp.config.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${PROJECT_DIR}"
 
 PROJECT_ID="julia-notebooks"
 ARTIFACT_REPOSITORY_NAME="julia-notebook-envs-repo"
